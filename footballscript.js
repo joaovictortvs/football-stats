28e98d8e8d5bee9fcdd17d9d003ddc2420f91f0b
joaovictortvs <tavaressjoaoo09@gmail.com>
2024-12-19 23:05:04 -0300

    Site versão 1 finalizado

--- footballscript.js
+++ footballscript.js
@@ -12,13 +12,9 @@ btn_ligas.forEach((el)=>{
         .then(res=>{
             let response = res
             const times = response.data.standings
-            console.log(times)
             times.forEach((el)=>{
                 createTable(el)
             })
-            // console.log(response.data.standings[0].stats) // pegar gamesplayed losses, pointDifferences, points, wins
-            // console.log(response.data.standings[0].team) // pegar o abbreviation e logo
-            // console.log(response.data.standings)
         })
 
     })
@@ -26,66 +22,69 @@ btn_ligas.forEach((el)=>{
 
 const btnVoltar = document.querySelector("#btnVoltarLigas")
 
+let pagina_inicial = true
 
 btnVoltar.addEventListener("click",()=>{
-    // const tabela_tbody = document.querySelector("#tabela_tbody") // parei aqui 
-    // tabela_tbody.remove()
+    pagina_inicial = true
+    const tbody = document.querySelector("#tbody")
+    tbody.innerHTML = " "
     const divLigas = document.querySelector("#divLigas")
     divLigas.classList.remove("ocultar")
     const divTabela = document.querySelector("#tabelaExibir")
     divTabela.classList.add("ocultar")
+   
 })
 
+
 const createTable=(time)=>{
-
-    const divLigas = document.querySelector("#divLigas")
-    divLigas.classList.add("ocultar")
-    const divTabela = document.querySelector("#tabelaExibir")
-    divTabela.classList.remove("ocultar")
-
-    const tabelaLiga = document.querySelector("#tabelaLiga")
-
-    const tbody = document.createElement("tbody")
-    tbody.setAttribute("id","tabela_tbody")
-    tabelaLiga.appendChild(tbody)
-
-    const tr_linha = document.createElement("tr")
-    tbody.appendChild(tr_linha)
-
-    const td_pos = document.createElement("td")
-    td_pos.innerHTML = time.stats[10].value
-    tr_linha.appendChild(td_pos)
+    if(pagina_inicial == true){
+        pagina_oficial = false
+        const divLigas = document.querySelector("#divLigas")
+        divLigas.classList.add("ocultar")
+        const divTabela = document.querySelector("#tabelaExibir")
+        divTabela.classList.remove("ocultar")
     
-    const td_imgName = document.createElement("td")
-    td_imgName.setAttribute("id","tdImg")
-    const div_img = document.createElement("div")
-    div_img.setAttribute("id","tdImagem")
-    const img_tdImgName = document.createElement("img")
-    img_tdImgName.setAttribute("src",time.team.logos[0])
-    div_img.appendChild(img_tdImgName)
-    td_imgName.appendChild(div_img)
+        const tbody = document.querySelector("#tbody")
+    
+        const tr_linha = document.createElement("tr")
+        tbody.appendChild(tr_linha)
+    
+        const td_pos = document.createElement("td")
+        td_pos.innerHTML = `${time.stats[10].value}°`
+        tr_linha.appendChild(td_pos)
+        
+        const td_imgName = document.createElement("td")
+        td_imgName.setAttribute("id","tdImg")
+        const div_img = document.createElement("div")
+        div_img.setAttribute("id","tdImagem")
+        const img_tdImgName = document.createElement("img")
+        img_tdImgName.setAttribute("src",time.team.logos[0].href)
+        div_img.appendChild(img_tdImgName)
+        td_imgName.appendChild(div_img)
+    
+        const div_tdImgName = document.createElement("div")
+        div_tdImgName.setAttribute("id","tdNome")
+        div_tdImgName.innerHTML = time.team.abbreviation
+        td_imgName.appendChild(div_tdImgName)
+        tr_linha.appendChild(td_imgName)
+    
+        const td_pontos = document.createElement("td")
+        td_pontos.innerHTML = time.stats[3].value
+        tr_linha.appendChild(td_pontos)
+    
+        const td_vitorias = document.createElement("td")
+        td_vitorias.innerHTML = time.stats[7].value
+        tr_linha.appendChild(td_vitorias)
+    
+        const td_derrotas = document.createElement("td")
+        td_derrotas.innerHTML = time.stats[1].value
+        tr_linha.appendChild(td_derrotas)
+    
+        const td_sdGols = document.createElement("td")
+        td_sdGols.innerHTML = time.stats[2].value
+        tr_linha.appendChild(td_sdGols)
+    }
 
-    const div_tdImgName = document.createElement("div")
-    div_tdImgName.setAttribute("id","tdNome")
-    div_tdImgName.innerHTML = time.team.abbreviation
-    td_imgName.appendChild(div_tdImgName)
-    tr_linha.appendChild(td_imgName)
-
-    const td_pontos = document.createElement("td")
-    td_pontos.innerHTML = time.stats[3].value
-    tr_linha.appendChild(td_pontos)
-
-    const td_vitorias = document.createElement("td")
-    td_vitorias.innerHTML = time.stats[7].value
-    tr_linha.appendChild(td_vitorias)
-
-    const td_derrotas = document.createElement("td")
-    td_derrotas.innerHTML = time.stats[1].value
-    tr_linha.appendChild(td_derrotas)
-
-    const td_sdGols = document.createElement("td")
-    td_sdGols.innerHTML = time.stats[2].value
-    tr_linha.appendChild(td_sdGols)
 }
 
 
